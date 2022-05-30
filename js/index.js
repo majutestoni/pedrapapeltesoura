a0f672534206836859dbb1481fb1e82794e71eb9
Maju Testoni <majuliatestoni@gmail.com>
2022-05-30 20:24:55 -0300

    correções

--- js/index.js
+++ js/index.js
@@ -6,27 +6,28 @@ let jogada = '';
 let jogadaOponente = '';
 
 for (let i = 0; i < 3; i++) {
-    if (jogada == '') {
-        escolhas[i].addEventListener('click', (evento) => {
+    escolhas[i].addEventListener('click', (evento) => {
+        if (jogada == '') {
             jogada = evento.target.value;
-            console.log(jogada)
-        })
-    }
+            escolhas[i].style.backgroundColor = '#D2691E';
+        }
+        ganhador();
+    })
 }
+
 function gerarOponente() {
 
+    let teste = Math.floor(Math.random() * 3 + 1)
     oponente.addEventListener('click', (event) => {
-        let teste = Math.floor(Math.random() * 3 + 1)
-        if (event.target.value == "oponente") {
-            if (jogadaOponente == '') {
-                if (teste == 1) {
-                    jogadaOponente = 'Pedra'
-                } else if (teste == 2) {
-                    jogadaOponente = 'Papel'
-                } else if (teste == 3) {
-                    jogadaOponente = 'Tesoura'
-                }
+        if (event.target.value == 'oponente') {
+            if (teste == 1) {
+                jogadaOponente = 'pedra'
+            } else if (teste == 2) {
+                jogadaOponente = 'papel'
+            } else if (teste == 3) {
+                jogadaOponente = 'tesoura'
             }
+
             event.target.value = jogadaOponente;
             ganhador();
         }
@@ -38,35 +39,37 @@ function gerarOponente() {
 function ganhador() {
 
     if (jogada != '' && jogadaOponente != '') {
-        switch (jogadaOponente) {
-            case 'Pedra':
-                if (jogada != 'Pedra' && jogada != 'Papel') {
-                    window.alert('oponente ganhou')
-                } else if (jogada != 'Pedra') {
-                    window.alert('voce ganhou')
-                } else {
-                    window.alert('empate')
-                }
-                break;
-            case 'Papel':
-                if (jogada != 'Pedra' && jogada != 'Papel') {
-                    window.alert('voce ganhou')
-                } else if (jogada != 'Pedra') {
-                    window.alert('empate')
-                } else {
-                    window.alert('oponente ganhou')
-                }
-                break;
-            case 'Tesoura':
-                if (jogada != 'Pedra' && jogada != 'Papel') {
-                    window.alert('empate')
-                } else if (jogada != 'Pedra') {
-                    window.alert('voce ganhou')
-                } else {
-                    window.alert('oponente ganhou')
-                }
-                break;
+
+        console.log(jogada)
+        console.log(jogadaOponente)
+
+        if (jogada == 'Tesoura' && jogadaOponente == 'pedra') {
+            window.alert('oponente ganhou')
+        } else if (jogada == 'Papel' && jogadaOponente == 'pedra') {
+            window.alert('voce ganhou')
+        } else if (jogada == 'Pedra' && jogadaOponente == 'pedra') {
+            window.alert('empate')
         }
+
+
+        if (jogada == 'Tesoura' && jogadaOponente == 'papel') {
+            window.alert('voce ganhou')
+        } else if (jogada == 'Papel' && jogadaOponente == 'papel') {
+            window.alert('empate')
+        } else if (jogada == 'Pedra' && jogadaOponente == 'papel') {
+            window.alert('oponente ganhou')
+        }
+
+
+        if (jogada == 'Tesoura' && jogadaOponente == 'tesoura') {
+            window.alert('empate')
+        } else if (jogada == 'Papel' && jogadaOponente == 'tesoura') {
+            window.alert('oponente ganhou')
+        } else if (jogada == 'Pedra' && jogadaOponente == 'tesoura') {
+            window.alert('voce ganhou')
+        }
+
     }
 }
+
 
